9d695f991f2c2c51c3a0c2a36d434c467515fa2a
Yldales <50544935+Yldales@users.noreply.github.com>
2024-06-25 22:31:26 +0200

    Remove tests URLs

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -174,19 +174,13 @@ function updateSelectedServer(serv){
 function updateBackgroundImage(serv = null) {
     
     // ...
-    let background_urls = [
-        'https://images.unsplash.com/photo-1580757468214-c73f7062a5cb'
-    ];
-
-    // ...
+    let background_urls = null;
     if (serv == null) {
         console.log('No server selected. Using default background.')
         let background_urls = Math.floor((Math.random() * fs.readdirSync(path.join(__dirname, 'app', 'assets', 'images', 'background_urls')).length));
     } else {
         console.log('Server selected. Using server background.')
-        let background_urls = [
-            'https://images.unsplash.com/photo-1580757468214-c73f7062a5cb'
-        ];
+        let background_urls = serv.rawServer.backgrounds;
     }
     // ...
     let background_url = background_urls[Math.floor(Math.random() * background_urls.length)];
